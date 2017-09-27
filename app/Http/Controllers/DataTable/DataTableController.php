29735e73f3a5322fe8194daf8cbfb5743a840be4
begimov <begimov@gmail.com>
2017-09-27 12:59:41 +0300

    index method on data table controller now returns all users

--- app/Http/Controllers/DataTable/DataTableController.php
+++ app/Http/Controllers/DataTable/DataTableController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\DataTable;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Database\Eloquent\Builder;
+use Exception;
 
 abstract class DataTableController extends Controller
 {
@@ -15,14 +16,26 @@ abstract class DataTableController extends Controller
     public function __construct()
     {
         $builder = $this->builder();
+
         if (!$builder instanceof Builder) {
             throw new Exception('Instance of Illuminate\Database\Eloquent\Builder required');
         }
+
+        $this->builder = $builder;
     }
 
     public function index()
     {
-        //
+        return response()->json([
+            'data' => [
+                'records' => $this->getRecords(),
+            ]
+        ]);
+    }
+
+    protected function getRecords()
+    {
+        return $this->builder->get();
     }
 
 }
