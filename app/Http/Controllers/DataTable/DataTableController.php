42d9fc62f4990c925596a87bf7b2911c73a0e152
begimov <begimov@gmail.com>
2017-09-27 12:35:37 +0300

    bas data table controller created

---
+++ app/Http/Controllers/DataTable/DataTableController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\DataTable;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DataTableController extends Controller
+{
+    //
+}
