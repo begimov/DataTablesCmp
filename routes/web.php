0bb283ac9e1bf50a23a7207a1691fce85fe1feb9
begimov <begimov@gmail.com>
2017-09-27 12:26:51 +0300

    admin users template and route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin/users', 'Admin\UserController@index')->name('admin.users.index');
